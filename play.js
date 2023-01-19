830e03ca0af641a7308f2430a8acc7c5537914ec
EuphieSS <samma118@hotmail.com>
2023-01-19 00:14:19 +0000

    Set up stdin to handle user keyboard input

--- play.js
+++ play.js
@@ -1,2 +1,22 @@
 const { net, connect } = require('./client');
 //imports the connect function using ES6 Shorthand syntax
+
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput); //event listener
+
+  return stdin;
+};
+
+const handleUserInput = function(key) { //runs when receive keyboard input from user
+    if (key === '\u0003') { // \u0003 maps to ctrl+c input
+      process.exit();
+    }
+  };
+
+setupInput();
